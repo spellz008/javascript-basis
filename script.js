748987c01c8c1463487d903ff1458ce61d7c4686
esther <chidinmaenwereji15@gmail.com>
2025-05-12 09:58:28 +0100

    add

---
+++ script.js
@@ -0,0 +1,57 @@
+// const month_in_a_year = 12
+// const year_of_birth = 2008
+// let current_year = 2025
+
+// let Myage = current_year - year_of_birth
+// console.log(Myage)
+
+// let Month_lived_on_earth = 12 * Myage
+// console.log(Month_lived_on_earth)
+
+// console.log("i will now count my chicken")
+// console.log("hens", 25 + 30 / 6)
+
+// console.log("roosters", 100 - 25 * 3 % 4)
+
+// console.log("now count the hens")
+
+// console.log(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 )
+
+// console.log(5 > 4)
+
+// console.log(16 == 4 * 4)
+
+// let myEyes = "blue"
+// let myHair = "black"
+
+// console.log("He has", myEyes, "eyes and", myHair, "hair")\
+
+
+
+let cars = 100
+let space_in_a_car = 4.0
+let drivers = 30
+let passengers = 90
+let cars_not_driven = cars - drivers 
+let cars_driven = drivers
+let carpool_capacity   = cars_driven * space_in_a_car
+let average_passengers_per_car = passengers / cars_driven
+
+console.log("there are", cars, "cars available")
+
+console.log("there are only", drivers, "drivers available")
+
+console.log("there will be", cars_not_driven, "empty cars today")
+
+console.log("we can transport", carpool_capacity, "people today")
+
+console.log("we have", passengers, "to carpool today")
+
+console.log("we need to put about", average_passengers_per_car, "in each car.")
+
+
+
+
+
+
+
---
+++ write.js
@@ -0,0 +1,21 @@
+let my_name = "zed A. shaw"
+let My_age = 35
+let my_height = 74
+let My_weight = 180
+let my_eyes = "blue"
+let my_teeth = "white"
+let my_hair = "brown"
+ 
+console.log("let's talk about", my_name)
+
+console.log("he's %d inches tall.", my_height)
+
+console.log("he's", My_weight,  "pounds heavy.", )
+
+console.log("Actually that's not too heavy")
+
+console.log("he's got",  my_eyes, "eyes and ", my_hair, "hair" )
+
+console.log("his teeth are usually white depending on the cofee")
+
+console.log("if i add",  My_age, my_height, My_weight, "i get", My_age + my_height + My_weight)
